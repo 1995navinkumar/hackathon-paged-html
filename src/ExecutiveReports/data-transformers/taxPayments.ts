1b95f7ef53eae89b9490beb1c619d0dc77bc6ff1
ankitdas13 <ankit.das@razorpay.com>
2023-07-06 19:32:30 +0530

    check tax paid status

--- src/ExecutiveReports/data-transformers/taxPayments.ts
+++ src/ExecutiveReports/data-transformers/taxPayments.ts
@@ -39,22 +39,24 @@ function getTaxPayments(data) {
 }
 
 
-function getTaxData(data){
-  const t = data.reduce((a, c)=>{
-   if('tds_category' in c.tax_payment.tds){   
-     a.push({
-         "category" : c.tax_payment.tds.tds_category.name,
-         "created_at" : dateFormatter.format(c.tax_payment.tds.created_at * 1000),
-         "total_tax_amount" : c.total_tax_amount
-     })
-   }
-   return a
-  },[])
+function getTaxData(data) {
+  const t = data.reduce((a, c) => {
+    if ('tds_category' in c.tax_payment.tds) {
+      if (c.tax_payment.status == "paid") {
+        a.push({
+          "category": c.tax_payment.tds.tds_category.name,
+          "created_at": dateFormatter.format(c.tax_payment.tds.created_at * 1000),
+          "total_tax_amount": c.total_tax_amount
+        })
+      }
+    }
+    return a
+  }, [])
 
   const groupBy = (a, k) => {
     let reduceData = a.reduce((result, currentValue) => {
       result[currentValue[k]] = result[currentValue[k]] || [];
-       result[currentValue[k]].push(
+      result[currentValue[k]].push(
         currentValue
       );
       delete currentValue.category
@@ -64,27 +66,27 @@ function getTaxData(data){
   };
 
   const groupedData = groupBy(t, 'category')
-  
+
   const tdsCategoryData = []
-  
-  for(let i in groupedData){
+
+  for (let i in groupedData) {
     let amount = 0
-    for(let j = 0; j < groupedData[i].length; j++){
-       amount += groupedData[i][j].total_tax_amount
+    for (let j = 0; j < groupedData[i].length; j++) {
+      amount += groupedData[i][j].total_tax_amount
     }
     tdsCategoryData.push({
-      "category" : i,
-      "total_amount" : amount
+      "category": i,
+      "total_amount": amount
     })
-  } 
- 
+  }
+
   return {
     type: 'bar',
     data: {
-      labels: tdsCategoryData.map(d=>d.category),
+      labels: tdsCategoryData.map(d => d.category),
       datasets: [
         {
-          data: tdsCategoryData.map(d=>d.total_amount),
+          data: tdsCategoryData.map(d => d.total_amount),
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
         },
       ],
@@ -92,8 +94,8 @@ function getTaxData(data){
     options: {
       responsive: true,
       plugins: {
-        legend:{
-         display:false
+        legend: {
+          display: false
         }
       },
     },
